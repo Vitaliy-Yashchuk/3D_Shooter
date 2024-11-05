67bd6d8daaaecc916933dba534422e4977883555
Vitalik Yashchuk <114389609+Vitaliy-Yashchuk@users.noreply.github.com>
2024-11-05 16:14:38 +0200

    add new animation for player and for aiming

--- 3D_Shooter_Game/Assets/Scripts/PlayerController.cs
+++ 3D_Shooter_Game/Assets/Scripts/PlayerController.cs
@@ -12,12 +12,14 @@ public class PlayerController : MonoBehaviour
     public float speed=5f, rotateSpeed=5f;
     public int _attackDamage = 25;
     private int _health=100;
+    private Animator _animator;
+    private bool _isFired=false;
 
     private void Awake()
     {
         _photonView=GetComponent<PhotonView>();
         _rb = GetComponent<Rigidbody>();
-
+        _animator = GetComponent<Animator>();
         if (!_photonView.IsMine)
         {
             Destroy(GetComponentInChildren<Camera>().gameObject);
@@ -37,8 +39,14 @@ public class PlayerController : MonoBehaviour
         if(!_photonView.IsMine)
             return;
         RotatePlayer();
-
         Shoot();
+        UpdateWalkingAnimation();
+        if (Input.GetMouseButtonDown(1))
+        {
+            if(!_isFired)_animator.SetTrigger("Fire");
+            else _animator.SetTrigger("NotFire");
+                _isFired=!_isFired;
+        }
     }
 
     private void Shoot()
@@ -97,5 +105,10 @@ public class PlayerController : MonoBehaviour
     private void MovePlayer()
     {
         _rb.MovePosition(transform.position + (transform.forward * Time.fixedDeltaTime * speed* Input.GetAxis("Vertical")));
+    }
+    private void UpdateWalkingAnimation()
+    {
+        bool isWalking = Mathf.Abs(Input.GetAxis("Vertical")) > 0.1f;
+        _animator.SetBool("IsWalking", isWalking);
     }
 }
